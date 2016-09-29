6543656cf17f0c65fa377d575e2283af5b53f58d
maniafish <mania_fish@163.com>
2016-09-29 16:49:02 +0800

    Bootstrap

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,12 @@
 from . import main
+from flask import render_template
 
 
-@main.route('/hello', methods=['GET', 'POST'])
-def hello_page():
-    return 'Hello World'
+@main.route('/', methods=['GET', 'POST'])
+def home_page():
+    return render_template('index.html')
+
+
+@main.route('/user/<name>', methods=['GET', 'POST'])
+def user_page(name):
+    return render_template('user.html', name=name)
