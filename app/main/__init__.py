bb0592e26efb471584051d939db87ab01d777741
maniafish <mania_fish@163.com>
2016-09-30 17:42:24 +0800

    static folder

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__,
+                 template_folder='templates',
+                 static_folder='static',
+                 static_url_path='/static/main')
 
 from . import views, errors
